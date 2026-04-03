30020106fc07bcd813cc530891992e24d95fc4a8
htmldesigner7 <htmldesigner7@gmail.com>
2026-04-03 14:17:05 +0530

    changes in get into touch redirct in contact us

--- resources/views/front/dashboard.blade.php
+++ resources/views/front/dashboard.blade.php
@@ -51,7 +51,7 @@
                         <li>A Legacy of Trust and Reliability Since 1938.</li>
                     </ul>
                     <!--<p class="text-white">Your Strategic Partner for Oil & Gas, Offshore, Onshore, Marine & Industrial Supply Solutions | Home to the Middle East's Largest Nonferrous Stock | A Legacy of Trust and Reliability Since 1938.</p>-->
-                    <a class="btn btn--ripple" id="ripple" href="{{ route('contact') }}">Get in Touch<svg
+                    <a class="btn btn--ripple" id="ripple" href="{{ route('contact') }}" onclick="localStorage.setItem('scrollToBuildTogether','1')">Get in Touch<svg
                             xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none">
                             <path d="M4.5 19.5L19.5 4.5M19.5 4.5H8.25M19.5 4.5V15.75" stroke="#fff" stroke-width="1.5"
                                 stroke-linecap="round" stroke-linejoin="round"></path>
